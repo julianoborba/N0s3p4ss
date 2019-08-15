942276094209b5b3ac0731138a17297be3fcccc6
Carlos Amarante <amarante.carlos@hotmail.com>
2019-08-15 14:18:25 -0300

    Check nginx last version (#9)
    * Create nginx logic and tests
    
    * Fix break lines

--- check_versions.py
+++ check_versions.py
@@ -5,3 +5,14 @@ def is_amazon_s3_server(server):
     version = bool(search(r'\d', server))
 
     return version and 'AmazonS3' in server
+
+
+def compare_nginx_version(server, nginx_version):
+    server_version = str((server.split('nginx/'))[1])
+
+    if server_version < nginx_version:
+        return ('The server nginx version is lesser than nginx expected \
+version; The expected version is {}'.format(nginx_version))
+
+    return ('The server nginx version is the same as expected \
+version; The expected version is {}'.format(nginx_version))
--- tests/test_check_versions.py
+++ tests/test_check_versions.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from check_versions import is_amazon_s3_server
+from check_versions import is_amazon_s3_server, compare_nginx_version
 
 
 class CheckVersionTest(TestCase):
@@ -15,3 +15,19 @@ class CheckVersionTest(TestCase):
     def test_that_should_not_recognize_aws_header(self):
         nginx = 'nginx/1.15.6'
         self.assertEqual(is_amazon_s3_server(nginx), False)
+
+    def test_that_should_recognize_nginx_last_version_header(self):
+        nginx_last_version = '1.16.1'
+        nginx_header_application = 'nginx/1.16.1'
+        self.assertEqual(compare_nginx_version(
+            nginx_header_application, nginx_last_version),
+            'The server nginx version is the same as expected version; \
+The expected version is 1.16.1')
+
+    def test_that_should_not_recognize_nginx_last_version_header(self):
+        nginx_last_version = '1.16.1'
+        nginx_header_application = 'nginx/1.14.1'
+        self.assertEqual(compare_nginx_version(
+            nginx_header_application, nginx_last_version),
+            'The server nginx version is lesser than nginx expected version; \
+The expected version is 1.16.1')
