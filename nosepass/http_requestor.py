cff53da3879c36a61897aaa05766c03a576d3e9b
Carlos Amarante <amarante.carlos@hotmail.com>
2019-09-13 16:30:19 -0300

    Organize http requestor (#30)

--- nosepass/http_requestor.py
+++ nosepass/http_requestor.py
@@ -1,4 +1,6 @@
 from requests.sessions import Session
+from requests.exceptions import HTTPError, ConnectionError
+from requests.models import Response
 from custom_json_logger import getLogger
 
 USER_AGENT = {
@@ -17,13 +19,21 @@ PROXIES = {
 def do_get(url):
     try:
         with Session() as session:
-            return session.get(
+            response = session.get(
                 headers=USER_AGENT,
                 proxies=PROXIES,
                 timeout=10,
-                url='https://www.google.com',
+                url=f'http://{url}',
                 verify=False
             )
+            response.raise_for_status()
+            return response
+
+    except HTTPError as http_error:
+        response = Response()
+        response.status_code = http_error.response.status_code
+        return response
+
     except ConnectionError as connection_error:
         getLogger().error(
             'ConnectionError for %s, cause %s',
@@ -31,4 +41,4 @@ def do_get(url):
             connection_error,
             exc_info=1
         )
-        return None
+        return Response()
--- tests/test_http_requestor.py
+++ tests/test_http_requestor.py
@@ -1,8 +1,9 @@
 from unittest import TestCase
 from unittest.mock import patch
 from requests.models import Response
-from http_requestor import do_get
 from requests.sessions import Session
+from requests.exceptions import ConnectionError
+from nosepass.http_requestor import do_get
 
 USER_AGENT = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) '
@@ -25,16 +26,18 @@ class HTTPRequestorTest(TestCase):
         response.status_code = 200
         get.return_value = response
 
-        response = do_get('https://www.google.com')
+        response = do_get('www.google.com')
         get.assert_called_once_with(
             headers=USER_AGENT,
             proxies=PROXIES,
             timeout=10,
-            url='https://www.google.com',
+            url='http://www.google.com',
             verify=False
         )
         self.assertEqual(200, response.status_code)
 
     @patch.object(Session, 'get', side_effect=ConnectionError)
-    def test_that_should_raises_connection_error(self, get):
-        self.assertRaises(ConnectionError, do_get('https://www.google.com'))
+    def test_that_should_not_raises_connection_error(self, get):
+        response = do_get('www.google.com')
+
+        self.assertIsNotNone(response)
